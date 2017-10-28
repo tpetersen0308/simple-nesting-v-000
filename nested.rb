e595ff2ac639ded74b3a988ea068ce1f5acca350
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 19:59:46 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -24,19 +24,21 @@ def alan_kay_is_known_for
 
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+   }
+
+	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
