883cbe4e41f5e3f72fab40bf9ce4aa0fe8377268
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 20:16:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 
 def hopper
 	programmer_hash =
@@ -87,7 +89,8 @@ def adding_matz
 	programmer_hash[:yukihiro_matsumoto] << {
 	   :known_for => "Ruby",
 	    :languages => ["LISP", "C"]
-	 }
+	}
+	binding.pry
 
 end
 
